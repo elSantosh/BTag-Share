3c6e67fec982ef8841896420306fb6e0572d6ed0
Santosh Guruju  | MACROKIOSK <santosh.guruju@macrokiosk.com>
2020-06-29 10:24:23 +0800

    Initial Commit

---
+++ BTag-Share/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BTag-Share
+//
+//  Created by Santosh Guruju | MACROKIOSK on 29/06/20.
+//  Copyright © 2020 Santosh Guruju | MACROKIOSK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
